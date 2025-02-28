d35abd4e6eef5c9d9d37f7454ef57d209e0beb6d
sunsheng <941216988@qq.com>
2025-02-28 09:59:29 +0800

    vant 4按需引入

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <van-button type="primary" ></van-button>
   </div>
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+// vant 按需引入
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from '@vant/auto-import-resolver';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({
+      resolvers: [VantResolver()],
+    }),
+    Components({
+      resolvers: [VantResolver()],
+    }),
   ],
   resolve: {
     alias: {
@@ -16,3 +25,4 @@ export default defineConfig({
     },
   },
 })
+

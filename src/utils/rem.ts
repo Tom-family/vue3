e46d73dea57602cb12264a016f5d1a1c2721f780
sunsheng <941216988@qq.com>
2025-03-04 17:06:03 +0800

    rem 防抖处理

--- src/utils/rem.ts
+++ src/utils/rem.ts
@@ -1,11 +1,15 @@
 // 设置 rem 函数
-function setRem():void {
-  const width:number = document.documentElement.clientWidth;
+function setRem(): void {
+  const width: number = document.documentElement.clientWidth;
   document.documentElement.style.fontSize = width / 10 + "px"; // 1rem = 192px，设计稿宽度为 192px
 }
 // 初始化
 setRem();
 //监听窗口变化 改变窗口大小时重新设置 rem
-window.onresize = function () {
-  setRem();
-};
+let resizeTimer: ReturnType<typeof setTimeout>;
+window.addEventListener("resize", function () {
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    setRem();
+  }, 200); // 200ms 防抖
+});

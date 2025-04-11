a6ef192969009c3bb38103d139e4d7d169be631f
sunsheng <941216988@qq.com>
2025-04-11 16:44:10 +0800

    添加下拉刷新

--- src/views/other/Other.vue
+++ src/views/other/Other.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="card-page">
-    <van-list v-model="loading" :finished="finished" :finished-text="finishedText" @load="getList" offset="30">
-      <div class="mine-list" v-for="(item, index) in list" :key="index">
-        <div style="color: #fff">{{ index }}</div>
-      </div>
-    </van-list>
+    <van-pull-refresh class="refresh-box" v-model="refreshing" @refresh="onRefresh">
+      <van-list v-model:loading="loading" :finished="finished" :finished-text="finishedText" @load="getList" offset="30">
+        <van-empty description="暂无数据" v-if="noData" />
+        <div class="page-item" v-for="(item, index) in list" :key="index">
+          <div style="color: #fff">{{ index }}</div>
+        </div>
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 <script setup lang="ts">
@@ -19,13 +22,21 @@ const loading = ref(false);
 const finished = ref(false);
 const finishedText = ref("没有更多了");
 const noData = ref(false);
+const refreshing = ref(false);
 
 const getList = async () => {
   let params = {
     pageNum: pageNum.value,
     pageSize: pageSize.value,
   };
+
   let res = await userList(params);
+  // 判断是否为下拉刷新
+  if (refreshing.value) {
+    refreshing.value = false;
+    list.value = [];
+  }
+
   list.value.push(...res.data.list);
   if (list.value.length == 0) {
     finishedText.value = "";
@@ -36,19 +47,37 @@ const getList = async () => {
   }
   pageNum.value++;
 
-  if (list.value.length >= res.data.total) {
+  if (list.value.length == res.data.total) {
     finished.value = true;
   } else {
     finished.value = false;
   }
+
   loading.value = false;
+};
+
+// 刷新
+const onRefresh = () => {
+  noData.value = false;
+  finished.value = false;
+  pageNum.value = 1;
+  // 将 loading 设置为 true，表示处于加载状态
+  loading.value = true;
+  getList();
 };
 </script>
 <style lang="scss" scoped>
-.mine-list {
-  width: 100%;
-  height: 200px;
-  background: red;
-  margin-bottom: 10px;
+.card-page {
+  height: 100%;
+  .refresh-box {
+    height: 100%;
+    overflow: auto;
+    .page-item {
+      width: 100%;
+      height: 200px;
+      background: red;
+      margin-bottom: 10px;
+    }
+  }
 }
 </style>
